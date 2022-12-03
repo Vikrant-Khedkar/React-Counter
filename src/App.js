c1b254702e439df4ef09e0b4ca8236edd1aa909d
vikrant-khedkar <vikrantkhedkar2720@gmail.com>
2022-12-04 00:27:21 +0530

    Made changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App(){
      <header> <h1>Counter App Using React States</h1></header>
      <h2>Current Value Of Count is {count}</h2>
      <button onClick={() => setCount(count + 1)}>Increase</button>
-     <button onClick={() => setCount(count - 1)}>Decrease</button>
+     <button onClick={() => (count <= 0 ? "": setCount( count - 1))}>Decrease</button>
      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
